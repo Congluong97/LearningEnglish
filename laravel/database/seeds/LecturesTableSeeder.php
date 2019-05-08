fe9a39a4f4fd88c8dd873a35d6fb49a1389ef703
bichSf <tranbichbk@gmail.com>
2019-05-08 16:47:53 +0700

    giao dien +csdl table lectures

---
+++ laravel/database/seeds/LecturesTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LecturesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('lectures')->insert(
+    		[
+    			[
+    				'id'=>1,
+    				'name'=>'First Snow Fall',
+    				'image'=>'c1.jpg',
+    				'level'=>'Level 1 - Beginner',
+    				'status'=>1
+    			],
+    			[
+    				'id'=>2,
+    				'name'=>'Jessica\'s First Day of School',
+    				'image'=>'c2.jpg',
+    				'level'=>'Level 1 - Beginner',
+    				'status'=>1
+    			],
+    			[
+    				'id'=>3,
+    				'name'=>'My Flower Garden',
+    				'image'=>'c3.jpg',
+    				'level'=>'Level 1 - Beginner',
+    				'status'=>1
+    			],
+    			[
+    				'id'=>4,
+    				'name'=>'Going Camping',
+    				'image'=>'c4.jpg',
+    				'level'=>'Level 1 - Beginner',
+    				'status'=>1
+    			]
+    		]
+    	);
+
+    }
+}
