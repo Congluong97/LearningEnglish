7b52debb883d9126c2344fa5ea224b98a9230451
dangngochust98 <43170458+dangngochust98@users.noreply.github.com>
2019-05-11 23:49:34 +0700

    ngoc

---
+++ laravel/resources/views/user/instructors.blade.php
@@ -0,0 +1,224 @@
+@extend('master')
+@section('content')
+
+
+    <!-- ##### Breadcumb Area Start ##### -->
+    <div class="breadcumb-area">
+        <!-- Breadcumb -->
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Courses</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Art &amp; Design</li>
+            </ol>
+        </nav>
+    </div>
+    <!-- ##### Breadcumb Area End ##### -->
+
+    <!-- ##### Instructors Video Start ##### -->
+    <div class="instructors-video d-flex align-items-center justify-content-center bg-img" style="background-image: url(img/bg-img/bg4.jpg);">
+        <h2>Be The Best English Teacher</h2>
+        <!-- video btn -->
+        <a href="https://www.youtube.com/watch?v=qC_T9ePzANg" class="video-btn"><i class="fa fa-play" aria-hidden="true"></i></a>
+    </div>
+    <!-- ##### Instructors Video End ##### -->
+
+    <!-- ##### Best Tutors Area Start ##### -->
+    <section class="best-tutors-area section-padding-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading">
+                        <h3>The Best Tutors in English</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="tutors-slide owl-carousel">
+
+                        <!-- Single Tutors Slide -->
+                        <div class="single-tutors-slides">
+                            <!-- Tutor Thumbnail -->
+                            <div class="tutor-thumbnail">
+                                <img src="img/congluong.jpg" alt="">
+                            </div>
+                            <!-- Tutor Information -->
+                            <div class="tutor-information text-center">
+                                <h5>Công Lương</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Single Tutors Slide -->
+                        <div class="single-tutors-slides">
+                            <!-- Tutor Thumbnail -->
+                            <div class="tutor-thumbnail">
+                                <img src="img/nguyenquangminh.jpg" alt="">
+                            </div>
+                            <!-- Tutor Information -->
+                            <div class="tutor-information text-center">
+                                <h5>Nguyễn Quang Minh</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Single Tutors Slide -->
+                        <div class="single-tutors-slides">
+                            <!-- Tutor Thumbnail -->
+                            <div class="tutor-thumbnail">
+                                <img src="img/trieuxuan.jpg" alt="">
+                            </div>
+                            <!-- Tutor Information -->
+                            <div class="tutor-information text-center">
+                                <h5>Triệu Thị Xuân</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Single Tutors Slide -->
+                        <div class="single-tutors-slides">
+                            <!-- Tutor Thumbnail -->
+                            <div class="tutor-thumbnail">
+                                <img src="img/tranbich.jpg" alt="">
+                            </div>
+                            <!-- Tutor Information -->
+                            <div class="tutor-information text-center">
+                                <h5>Trần Thị Bích</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Single Tutors Slide -->
+                        <div class="single-tutors-slides">
+                            <!-- Tutor Thumbnail -->
+                            <div class="tutor-thumbnail">
+                                <img src="img/dangngoc.jpg" alt="">
+                            </div>
+                            <!-- Tutor Information -->
+                            <div class="tutor-information text-center">
+                                <h5>Đặng Thị Ngọc</h5>
+                                <span>Teacher</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum laoreet elit, sit amet tincidunt mauris ultrices vitae.</p>
+                                <div class="social-info">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Best Tutors Area End ##### -->
+
+    <!-- ##### Top Teacher Area Start ##### -->
+    <section class="top-teacher-area section-padding-0-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading">
+                        <h3>Top Teachers in Every Field</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Single Teacher -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="img/congluong.jpg" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Công Lương</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Teacher -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="img/nguyenquangminh2.jpg" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Nguyễn Quang Minh</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Teacher -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="img/trieuxuan.jpg" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Triệu Thị Xuân</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Teacher -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="img/tranbich2.jpg" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Trần Thị Bích</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Teacher -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-instructor d-flex align-items-center mb-30">
+                        <div class="instructor-thumb">
+                            <img src="img/dangngoc.jpg" alt="">
+                        </div>
+                        <div class="instructor-info">
+                            <h5>Đặng Thị Ngọc</h5>
+                            <span>Teacher</span>
+                        </div>
+                    </div>
+                </div>
+
+               
+    <!-- ##### Top Teacher Area End ##### -->
+@endsection()
+
+  
